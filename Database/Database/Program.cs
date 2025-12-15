98970a8ebfd3e07c3b65791d8d009ce64ee55cc7
Sean Mooney <seanmooney1996@gmail.com>
2025-12-15 17:15:59 +0000

    Run migrations on service startup

--- Database/Database/Program.cs
+++ Database/Database/Program.cs
@@ -92,6 +92,22 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<PropertyServiceContext>();
+        context.Database.Migrate(); 
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
